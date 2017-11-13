e2d72d3c8d59282d4f7f036892739311fd8732f1
OMAR HEGAZI <edgeofdarkness_x1@hotmail.com>
2017-11-13 23:58:44 +0200

    error in the new fireStore auth and list method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+// import { AngularFireModule } from 'angularfire2';
+// import { AngularFireAuth } from 'angularfire2/auth';
+// import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: Observable<any>;
+  name: any;
+  msgVal = '';
+
+  constructor(public afdb: AngularFirestore, private auth: AngularFireAuth) {
+    this.items = afdb.collection('/messages').valueChanges();
+
+    this.afdb.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+// import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+
+
+
+  // Initialize Firebase
+  export const firebaseConfig = {
+    apiKey: 'AIzaSyD-2Evt-L1eV5zAxtXvsz0CaBTh9C0NYs4',
+    authDomain: 'what-we-do-1be54.firebaseapp.com',
+    databaseURL: 'https://what-we-do-1be54.firebaseio.com',
+    projectId: 'what-we-do-1be54',
+    storageBucket: '',
+    messagingSenderId: '609482384184'
+  };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    // AngularFireDatabaseModule
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
